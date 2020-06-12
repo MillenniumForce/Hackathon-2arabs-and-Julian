f85f131fcd30c8e38628a1a542808657575fb51c
MillenniumForce <57126005+MillenniumForce@users.noreply.github.com>
2020-06-13 00:50:45 +1000

    Add files via upload

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 import sys
+import threading
 import time
-# Pandas must be downloaded seperately if it hasn't already been downloaded (either through pip or anaconda)
 import pandas as pd
 
 def main():
@@ -11,57 +11,67 @@ def main():
     print(r" |_|  |_\___\__,_|\___\___/_||_|_||_\___\__|\__|")
     print("\n")
 
-    print("[1] Show clients") #! Check
-    print("[2] Add patients") #! CHECK
-    print("[3] Remove patients")
-    print("[4] Send reminder to patient")
-    print("[5] Send scheduled reminder to patients")
+    print("[1] Show clients") #! DONE
+    print("[2] Add patients") #! DONE
+    print("[3] Remove patients") #TODO: NOT FUNCTIONAL
+    print("[4] Send reminder to patient") #! DONE
+    print("[5] Send scheduled reminder to patients") #! DONE
     print("[6] Exit")
     
-    input = get_input()
-    while (input != 6):
-        if input == 1: show_patient()
-        if input == 2: add_patient()
-        input = get_input() 
-    
-    #call_client()
-
+    user_input = get_input() 
+    while (user_input != 6):
+        if user_input == 1: show_patient()
+        if user_input == 2: add_patient()
+        if user_input == 3: remove_patient()
+        if user_input == 4: call_client(user_input)
+        if user_input == 5: call_client(user_input)
+        if user_input == 0: print("[*] Please try again")
+        user_input = get_input()
 def get_input():
     try:        
-        option = int(input("> "))
-        if option not in range(1, 7):
+        user_input = int(input("> "))
+        if user_input not in range(1, 7):
             print("[*] Please enter a number from 1 to 5")  
-            get_input()
+            return 0
     except ValueError:
         print("[*] Please enter Integers only")
-        get_input()
+        return 0
     except KeyboardInterrupt:
         print("\n[*] Exiting")
         sys.exit(1)
     except EOFError:
         print("\n[*] Exiting")
         sys.exit(1)
-    return option
+    return user_input
 
 def show_patient():
-    import pandas as pd 
-    df = pd.read_csv('patient_data.csv')
-    print(df)
+    try: 
+        df = pd.read_csv('patient_data.csv')
+        print(df)
+    except IOError:
+        print("[*] There are no records")
+        return
+    
 
 def add_patient():
-    first_name = str(input("First Name: "))
-    last_name = str(input("Surname: "))
-    location = str(input("Location: "))
-    prescription = str(input("Prescription: "))
-    time = str(input("Dose Time: "))
-    dose_size = int(input("Dose size: "))
+    try:
+        first_name = str(input("[*] First Name: "))
+        last_name = str(input("[*] Surname: "))
+        location = str(input("[*] Location: "))
+        prescription = str(input("[*] Prescription: "))
+        time = str(input("[*] Dose Time: "))
+        dose_frequency = int(input("[*] Dose Frequency (mg): "))
+    except KeyboardInterrupt:
+        return
+    except EOFError: 
+        return
 
-    data = {'Name': [name],
+    data = {'First Name': [first_name],
         'Surname': [last_name],
         'Location': [location],
        	'Prescription': [prescription],
        	'Time': [time],
-       	'Dose Size (mg)': [dose_size]}
+       	'Dose Frequency (mg)': [dose_frequency]}
     
     try: 
         f = open('patient_data.csv')
@@ -73,25 +83,56 @@ def add_patient():
     if f != False: df.to_csv('patient_data.csv', mode='a', index=False, header=False)
     else: df.to_csv('patient_data.csv', mode='a', index=False, header=True)
 
-def call_client():
+def remove_patient():
+    df = pd.read_csv('patient_data.csv')
+    
+    try: 
+        i = int(input("[*] Enter a number from 0 to %d indicating the row index: " % (len(df)-1)))
+        if i not in range(0,len(df)): return 
+    except KeyboardInterrupt:
+        return
+    except EOFError:
+        return
+    except TypeError:
+        return
+    df.drop([i,0], inplace = True)
+
+
+def call_client(user_input):
     host = 'localhost'
     port = 10000
     serversocket = socket.socket()
-    serversocket.bind((host, port))
-    serversocket.listen(1)
-    print("[*] Waiting to connect to client")
+    try: 
+        serversocket.bind((host, port))
+        serversocket.listen(1)
+    except OSError:
+        print("[*] Server is cooling down try again later")
+        return
+    print("[*] Waiting to connect to patient")
 
     try:
         conn, addr = serversocket.accept()
         print("[*] Got connection from %s" % (str(addr)))
-        msg = input() + "\r\n"
-        # time.sleep(5)
-        conn.send(msg.encode('ascii'))
-        conn.close()
+        if (user_input == 4):
+            msg = input("Msg: ") + "\r\n"
+            conn.send(msg.encode('ascii'))
+            conn.close()
+        elif (user_input == 5):
+            time_sec = int(input("Enter seconds: "))
+            time_min = int(input("Enter minutes: ")) * 60
+            time_hours = int(input("Enter hours: ")) * 3600
+            user_time = time_sec + time_min + time_hours
+            t = threading.Thread(target = scheduled_msg, args = (conn,user_time))
+            t.start()
     except KeyboardInterrupt:
         print("\n[*] Disconnecting")
         sys.exit(1)
 
+def scheduled_msg(conn,user_time):
+    msg = input("Scheduled Msg: ") + "\r\n"
+    time.sleep(user_time)
+    conn.send(msg.encode('ascii'))
+    conn.close()
 
 if __name__ == '__main__':
     main()
