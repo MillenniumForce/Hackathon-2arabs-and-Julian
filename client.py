380f5e85415294526d5b183d91da6c292b3ef755
MillenniumForce <57126005+MillenniumForce@users.noreply.github.com>
2020-06-13 08:56:30 +1000

    Ready to submit

--- client.py
+++ client.py
@@ -1,14 +1,23 @@
 import socket 
 
+# MEDCONNECT was created by Julian Garratt, Aimen Hamed, and Hossion Ali. To run the program use 'python3 server.py'. 
+# If 4 or 5 is used while the program is running you should be prompted with '[*] Waiting to connect to patient'.
+# At this point run 'python3 client.py' on a seperate terminal. You should then recieve 'Got connection from ...'
+# Any issues or bugs please contact Julian Garratt.
+
+# Create a new socket and bind it to the local host under port  10 000, any other port can be used 
+# make sure the new port if changed is above 1023 on both programs (0 to 1023 is restricted)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = 'localhost'
 port = 10000
 
+# Attempt connection to host
 try:
     s.connect((host, port))
+    s.settimeout(100) #timeout set to 100 seconds
     msg = s.recv(1024)
-    print('[*] From the doctor ' + msg.decode('ascii'))
+    print('[*] From the doctor: ' + msg.decode('ascii'))
     s.close()
 except socket.timeout:
-    print("[*] No connection between client and server")
+    print("[*] No connection between client and server or took too long to connect")
     s.close()
--- server.py
+++ server.py
@@ -4,6 +4,13 @@ import threading
 import time
 import pandas as pd
 
+# MEDCONNECT was created by Julian Garratt, Aimen Hamed, and Hossion Ali. To run the program use 'python3 server.py'. 
+# If 4 or 5 is used while the program is running you should be prompted with '[*] Waiting to connect to patient'.
+# At this point run 'python3 client.py' on a seperate terminal. You should then recieve 'Got connection from ...'
+# Any issues or bugs please contact Julian Garratt.
+
+# Note: pandas is an external library please download via 'python3 -m pip pandas'
+
 def main():
     print(r"  __  __        _  ___                      _   ")
     print(r" |  \/  |___ __| |/ __|___ _ _  _ _  ___ __| |_ ")
@@ -11,14 +18,15 @@ def main():
     print(r" |_|  |_\___\__,_|\___\___/_||_|_||_\___\__|\__|")
     print("\n")
 
-    print("[1] Show clients") #! DONE
-    print("[2] Add patients") #! DONE
-    print("[3] Remove patients") #TODO: NOT FUNCTIONAL
-    print("[4] Send reminder to patient") #! DONE
-    print("[5] Send scheduled reminder to patients") #! DONE
+    print("[1] Show clients") 
+    print("[2] Add patients") 
+    print("[3] Remove patients") 
+    print("[4] Send reminder to patient")  
+    print("[5] Send scheduled reminder to patients") 
     print("[6] Exit")
-    
-    user_input = get_input() 
+
+    #Get Input from user
+    user_input = get_input()
     while (user_input != 6):
         if user_input == 1: show_patient()
         if user_input == 2: add_patient()
@@ -27,11 +35,13 @@ def main():
         if user_input == 5: call_client(user_input)
         if user_input == 0: print("[*] Please try again")
         user_input = get_input()
+
+# Function gets input from user handling exception cases
 def get_input():
-    try:        
+    try:
         user_input = int(input("> "))
         if user_input not in range(1, 7):
-            print("[*] Please enter a number from 1 to 5")  
+            print("[*] Please enter a number from 1 to 5")
             return 0
     except ValueError:
         print("[*] Please enter Integers only")
@@ -44,15 +54,17 @@ def get_input():
         sys.exit(1)
     return user_input
 
+# Calls pandas and prints the dataframe. Note that if the dataframe is large enough it might fill up a substantial part of the terminal
 def show_patient():
-    try: 
+    try:
         df = pd.read_csv('patient_data.csv')
-        print(df)
+        if df.empty: print("[*] There are no records")
+        else: print(df)
     except IOError:
         print("[*] There are no records")
         return
-    
 
+# Add patients via a pandas dataframe handling exceptions 
 def add_patient():
     try:
         first_name = str(input("[*] First Name: "))
@@ -63,7 +75,10 @@ def add_patient():
         dose_frequency = int(input("[*] Dose Frequency (mg): "))
     except KeyboardInterrupt:
         return
-    except EOFError: 
+    except EOFError:
+        return
+    except Exception as e:
+        print("[?] Incorrect Value due to %s. Please try again\n[?] Note that 'Dose Frequency' must be a number" % (e))
         return
 
     data = {'First Name': [first_name],
@@ -73,31 +88,63 @@ def add_patient():
        	'Time': [time],
        	'Dose Frequency (mg)': [dose_frequency]}
     
-    try: 
+    # Special case where the csv file might not even exist. In that case create a new file instead of appending
+    try:
         f = open('patient_data.csv')
         f.close()
     except IOError:
         f = False
 
     df = pd.DataFrame(data)
-    if f != False: df.to_csv('patient_data.csv', mode='a', index=False, header=False)
+    if f != False and df.empty: df.to_csv('patient_data.csv', index=False)
+    if f != False: df.to_csv(
+        'patient_data.csv', mode='a', index=False, header=False)
     else: df.to_csv('patient_data.csv', mode='a', index=False, header=True)
 
+# Removes patients according to their row index (starts at 0). Might be a bit buggy where in some cases 
+# when after all rows are removed it displays 'Unnamed: 0' in the new dataframe
 def remove_patient():
     df = pd.read_csv('patient_data.csv')
-    
-    try: 
-        i = int(input("[*] Enter a number from 0 to %d indicating the row index: " % (len(df)-1)))
-        if i not in range(0,len(df)): return 
+
+    if df.empty:
+        print("[*] There is nothing to remove")
+        return
+
+    try:
+        i = int(input(
+            "[*] Enter a number from 0 to %d indicating the row index: " % (len(df)-1)))
+        if i not in range(0, len(df)): 
+            print("[*] Please enter a number within range")
+            return
     except KeyboardInterrupt:
         return
     except EOFError:
         return
     except TypeError:
         return
-    df.drop([i,0], inplace = True)
 
+    try: 
+        confirm = str(input("[*] Please confirm yes/no if you would like to delete row %d: " % (i)))
+        if confirm != 'yes' and confirm != 'no': 
+            print("[*] Please enter yes or no to confirm")
+            return 
+        elif confirm == 'no':
+            print("[*] Deletion Cancelled")
+            return 
+    except KeyboardInterrupt:
+        return
+    except EOFError:
+        return
+    except TypeError:
+        return
 
+    df.drop(i, inplace = True)
+    df.to_csv('patient_data.csv', index=False)
+
+# Function handles all server, socket, and threading
+# Will call local host waiting for a connection until client.py is executed
+# In built functionality includes either sending a message instantly, or a delayed message using threading
+# Cooldown is needed if user tries to run the server to quickly after prior usage
 def call_client(user_input):
     host = 'localhost'
     port = 10000
@@ -128,6 +175,7 @@ def call_client(user_input):
         print("\n[*] Disconnecting")
         sys.exit(1)
 
+# Background process function is called via threading (see call_client() function for implementation)
 def scheduled_msg(conn,user_time):
     msg = input("Scheduled Msg: ") + "\r\n"
     time.sleep(user_time)
